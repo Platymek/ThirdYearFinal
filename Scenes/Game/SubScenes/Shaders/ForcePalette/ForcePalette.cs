eaa2649274902522b188d6024870392b086d8e4f
Platymek <hectordavid.selby@gmail.com>
2024-01-12 11:41:20 +0000

    player model finished

--- Scenes/Game/SubScenes/Shaders/ForcePalette/ForcePalette.cs
+++ Scenes/Game/SubScenes/Shaders/ForcePalette/ForcePalette.cs
@@ -2,6 +2,7 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
+[Tool]
 public partial class ForcePalette : CanvasLayer
 {
 	public ShaderMaterial Filter;
@@ -11,7 +12,6 @@ public partial class ForcePalette : CanvasLayer
 	public override void _Ready()
 	{
 		Filter = (ShaderMaterial)GetNode<ColorRect>("ColorRect").Material;
-		Filter.SetShaderParameter("colors", Variant.From(_colors));
 
 		//ResetFilter();
 	}
@@ -24,12 +24,13 @@ public partial class ForcePalette : CanvasLayer
 		Filter.SetShaderParameter("c1", Variant.From(s.Colours1[s.CurrentPalette]));
 		Filter.SetShaderParameter("c2", Variant.From(s.Colours2[s.CurrentPalette]));
 	}
+*/
 
 	public override void _Process(double delta)
 	{
 		base._Process(delta);
 
-		ResetFilter();
+		Filter.SetShaderParameter("colors", Variant.From(_colors));
 	}
-*/
+
 }
